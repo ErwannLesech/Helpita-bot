6bdd42f260826c1a466309837c75c6f8f7bce483
erwann lesech <erwann.lesech@epita.fr>
2023-11-29 19:18:55 +0100

    fix: Intent defaults upgraded to all

--- src/main.py
+++ src/main.py
@@ -4,37 +4,37 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+load_dotenv(dotenv_path="config.conf")
 
-def main():
-    # loads the token
-    load_dotenv(dotenv_path="config.conf")
+intent = discord.Intents.all()
+intent.members = True
 
-    """
-    class HelpitaBot(commands.Bot):
-        def __init__(self):
-            super().__init__(command_prefix="/", intents=discord.Intents.default())
+class HelpitaBot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix="!", intents=intent)
 
-        # prints ready when the bot is started
-        async def on_ready(self):
-            print(f"{self.user.display_name} connected")
+    async def on_ready(self):
+        print("READY")
+    
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        
+        await self.process_commands(message)
+    
+        print(message.content)
+        if message.content.lower() == "bonjour":
+            await message.channel.send("Bonsoir")
+        elif message.content.startswith("/zeus"):
+            await message.channel.send("zeus mode")
+        elif message.content.startswith("/news"):
+            await message.channel.send("news mode")
+        elif message.content.startswith("/todo"):
+            await message.channel.send("todo mode")
+        
+    async def todo(ctx):
+        await ctx.send("todo mode")
 
-    help_bot = HelpitaBot()
-    help_bot.run(os.getenv("TOKEN"))
-    """
 
-    bot = commands.Bot(command_prefix="/", intents=discord.Intents.default())
-
-    @bot.event
-    async def on_ready():
-        print("connected")
-
-    # reacts when a message "/todo day task" is sent on the server
-    @bot.command(name="todo")
-    async def todo_list(context):
-        print("received")
-        await context.content.send("```day: task```")
-
-    bot.run(os.getenv("TOKEN"))
-
-if __name__ == "__main__":
-    main()
+bot = HelpitaBot()
+bot.run(os.getenv("TOKEN"))
